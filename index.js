9d858d077ab682564a8d0fed78376a5fa546aee2
Dakota Nelson <kotalilyy@gmail.com>
2021-07-29 14:47:22 -0500

    pseudocode, create employee class

---
+++ index.js
@@ -0,0 +1,18 @@
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+// function for manager prompt
+
+// function to add role
+
+// function to add engineer
+
+//function to add intern
+
+// empty array for employees
+
+// call manager prompt
