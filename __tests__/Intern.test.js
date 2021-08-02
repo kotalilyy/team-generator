e85a23235dc45504dc445a569b7a9283dd2bbcdf
Dakota Nelson <kotalilyy@gmail.com>
2021-08-02 07:11:56 -0500

    create more working tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -5,3 +5,15 @@ test("Can set school via constructor", () => {
     const e = new Intern("Sarah", 1, "test@test.com", testValue, "Intern");
     expect(e.school).toBe(testValue);
 });
+
+test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern";
+    const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+    expect(e.getRole()).toBe(testValue);
+  });
+  
+  test("Can get school via getSchool()", () => {
+    const testValue = "UCLA";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+  });
